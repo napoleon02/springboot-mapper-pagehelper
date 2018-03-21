6251f10e367b714414f60fd44466beda214d1365
岳耀廷 <463042340@qq.com>
2018-03-21 15:09:28 +0800

    first commit init a project by napoleon

---
+++ src/main/java/com/etoak/springboot/springbootmapperpagehelper/SpringbootMapperPagehelperApplication.java
@@ -0,0 +1,12 @@
+package com.etoak.springboot.springbootmapperpagehelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMapperPagehelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMapperPagehelperApplication.class, args);
+	}
+}
---
+++ src/test/java/com/etoak/springboot/springbootmapperpagehelper/SpringbootMapperPagehelperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.etoak.springboot.springbootmapperpagehelper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootMapperPagehelperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
